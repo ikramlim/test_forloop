4e949e4a66123d129cfc3432dd450f73f0fc0999
Ikram_Lim <ikramlim@gmail.com>
2015-12-19 00:10:43 +0800

    test_for_loop
    for loop test

---
+++ test_forloop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_forloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int j = 6;
+
+            for(i=1; i<=5; i++)
+            {
+                Console.Write(i + "/5= " + i / 5.0 + "\n");
+            }
+            Console.Write("---------------------------\n");
+
+            for(j=6;j<=10;j++)
+            {
+                Console.Write(j + "/5 = " + j / 5.0 +"\n");
+            }
+            Console.WriteLine("==========================\n");
+        }
+    }
+}
